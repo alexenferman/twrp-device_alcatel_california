9003465968ee2c9278ac4ffc3dbb94c9af539799
alexenferman <alex@alexenferman.com>
2021-01-14 20:30:31 -0500

    Re-arrange the lines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,41 +1,65 @@
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 DEVICE_PATH := device/alcatel/california
 
-TARGET_BOOTLOADER_BOARD_NAME := california
-TARGET_NO_BOOTLOADER := true
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
 
-TARGET_BOARD_PLATFORM := mt6572
-
+# Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a7
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := california
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := california
+TARGET_NO_BOOTLOADER := true
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true #Maybe not lol
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 6291456 # This is the maximum known partition size, but it can be higher, so we just omit it
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Kernel
 TARGET_PREBUILT_KERNEL := device/alcatel/california/prebuilt/zImage
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := device/alcatel/california/bootimg.mk
 BOARD_CUSTOM_MKBOOTIMG := mtkbootimg
-BOARD_KERNEL_BASE := 0x10000000 # Maybe Needs Fixing
+BOARD_KERNEL_BASE := 0x10000000
 BOARD_PAGE_SIZE := 2048
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_RAMDISK_OFFSET := 0x01000000 # Maybe Needs Fixing
-BOARD_SECOND_OFFSET := 0x00f00000
+BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_TAGS_OFFSET := 0x00000100
 
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# For building with TWRP minimal manifest
-ALLOW_MISSING_DEPENDENCIES := true
-
-BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_RECOVERY_FSTAB := device/alcatel/california/recovery/root/etc/recovery.fstab
+# Platform
+TARGET_BOARD_PLATFORM := mt6572
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
+# TWRP Configuration
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_THEME := portrait_hdpi
